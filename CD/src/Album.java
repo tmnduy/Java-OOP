d9b9e5a14feaf7e69589b3d44df3207c9e42ce4c
Tmnduy <tmnduy@gmail.com>
2020-11-21 01:20:40 +0700

    Finish AlbumCD OOP Java

--- CD/src/Album.java
+++ CD/src/Album.java
@@ -8,23 +8,6 @@ public class Album {
     protected double giaTien;
     protected Scanner input = new Scanner(System.in);
 
-    protected void __Input() {
-        System.out.println("Nhập mã CD: ");
-        maCD = input.nextInt();
-        System.out.println("Nhập tên CD: ");
-        tenCD = input.next();
-        System.out.println("Nhập tên ca sỹ: ");
-        casy = input.next();
-        System.out.println("Nhập số bài hát: ");
-        soBH = input.nextInt();
-        System.out.println("Nhập giá tiền: ");
-        giaTien = input.nextDouble();
-    }
-
-    public void __Output() {
-        System.out.printf("%-10d %-20s %-20s %-20d %-40.1f \n", maCD, tenCD, casy, soBH, giaTien);
-    }
-
     public Album() {
     }
 
@@ -33,46 +16,6 @@ public class Album {
         this.tenCD = tenCD;
         this.casy = casy;
         this.soBH = soBH;
-        this.giaTien = giaTien;
-    }
-
-    public int getMaCD() {
-        return maCD;
-    }
-
-    public void setMaCD(int maCD) {
-        this.maCD = maCD;
-    }
-
-    public String getTenCD() {
-        return tenCD;
-    }
-
-    public void setTenCD(String tenCD) {
-        this.tenCD = tenCD;
-    }
-
-    public String getCasy() {
-        return casy;
-    }
-
-    public void setCasy(String casy) {
-        this.casy = casy;
-    }
-
-    public int getSoBH() {
-        return soBH;
-    }
-
-    public void setSoBH(int soBH) {
-        this.soBH = soBH;
-    }
-
-    public double getGiaTien() {
-        return giaTien;
-    }
-
-    public void setGiaTien(double giaTien) {
         this.giaTien = giaTien;
     }
 
--- CD/src/App.java
+++ CD/src/App.java
@@ -22,7 +22,7 @@ public class App {
                     for (int i = 0; i < n; i++) {
                         System.out.println("CD thứ " + (i + 1) + ": ");
                         arrAlbum[i] = new Album();
-                        arrAlbum[i].__Input();
+                        __Input(arrAlbum[i]);
                     }
                     break;
                 case 2:
@@ -32,74 +32,82 @@ public class App {
                         System.out.printf("%-10s %-20s %-20s %-20s %-40s \n", "Mã CD", "Tên CD", "Tên ca sỹ",
                                 "Số bài hát", "Giá tiền");
                         for (int i = 0; i < n; i++) {
-                            arrAlbum[i].__Output();
+                            __Output(arrAlbum[i]);
                         }
                     }
                     break;
                 case 3:
-                    if (arrAlbum.length == 0) {
-                        System.out.println("Album trống, chọn 1 để nhập thông tin CD");
-                    } else {
-                        double sum = 0;
-                        for (int i = 0; i < n; i++) {
-                            sum += arrAlbum[i].getGiaTien();
-                        }
-                        System.out.println("Tổng giá tiền: " + sum);
-                    }
+                    // if (arrAlbum.length == 0) {
+                    // System.out.println("Album trống, chọn 1 để nhập thông tin CD");
+                    // } else {
+                    // double sum = 0;
+                    // for (int i = 0; i < n; i++) {
+                    // sum += arrAlbum[i].giaTien;
+                    // }
+                    // System.out.println("Tổng giá tiền: " + sum);
+                    // }
 
+                    __Sum(arrAlbum, n);
                     break;
                 case 4:
                     System.out.println(arrAlbum.length);
                     break;
                 case 5:
-                    if (arrAlbum.length == 0) {
-                        System.out.println("Album trống, chọn 1 để nhập thông tin CD");
-                    } else {
-                        Album temp = arrAlbum[0];
-                        for (int i = 0; i < n - 1; i++) {
-                            for (int j = i + 1; j < n; j++) {
-                                if (arrAlbum[i].getGiaTien() < arrAlbum[j].getGiaTien()) {
-                                    temp = arrAlbum[j];
-                                    arrAlbum[j] = arrAlbum[i];
-                                    arrAlbum[i] = temp;
-                                }
-                            }
-                        }
-                        System.out.println("Sắp xếp hoàn tất, nhấn 2 để xem lại danh sách.");
-                    }
+                    // if (arrAlbum.length == 0) {
+                    // System.out.println("Album trống, chọn 1 để nhập thông tin CD");
+                    // } else {
+                    // Album temp = arrAlbum[0];
+                    // for (int i = 0; i < n - 1; i++) {
+                    // for (int j = i + 1; j < n; j++) {
+                    // if (arrAlbum[i].giaTien < arrAlbum[j].giaTien) {
+                    // temp = arrAlbum[j];
+                    // arrAlbum[j] = arrAlbum[i];
+                    // arrAlbum[i] = temp;
+                    // }
+                    // }
+                    // }
+                    // System.out.println("Sắp xếp hoàn tất, nhấn 2 để xem lại danh sách.");
+                    // }
+
+                    __SortMoney(arrAlbum, n);
                     break;
                 case 6:
-                    if (arrAlbum.length == 0) {
-                        System.out.println("Album trống, chọn 1 để nhập thông tin CD");
-                    } else {
-                        Album temp = arrAlbum[0];
-                        for (int i = 0; i < n - 1; i++) {
-                            for (int j = i + 1; j < n; j++) {
-                                if (arrAlbum[i].getTenCD().compareTo(arrAlbum[j].getTenCD()) > 0) {
-                                    temp = arrAlbum[j];
-                                    arrAlbum[j] = arrAlbum[i];
-                                    arrAlbum[i] = temp;
-                                }
-                            }
-                        }
-                        System.out.println("Sắp xếp hoàn tất, nhấn 2 để xem lại danh sách.");
-                    }
+                    // if (arrAlbum.length == 0) {
+                    // System.out.println("Album trống, chọn 1 để nhập thông tin CD");
+                    // } else {
+                    // Album temp = arrAlbum[0];
+                    // for (int i = 0; i < n - 1; i++) {
+                    // for (int j = i + 1; j < n; j++) {
+                    // if (arrAlbum[i].tenCD.compareTo(arrAlbum[j].tenCD) > 0) {
+                    // temp = arrAlbum[j];
+                    // arrAlbum[j] = arrAlbum[i];
+                    // arrAlbum[i] = temp;
+                    // }
+                    // }
+                    // }
+                    // System.out.println("Sắp xếp hoàn tất, nhấn 2 để xem lại danh sách.");
+                    // }
+
+                    __SortName(arrAlbum, n);
                     break;
                 case 7:
-                    System.out.println("Nhập tên CD muốn xóa: ");
-                    String nameRemove = input.next();
-                    for (int i = 0; i < n; i++) {
-                        if (arrAlbum[i].getTenCD().equals(nameRemove)) {
-                            for (int j = i; j < n - 1; j++) {
-                                arrAlbum[j] = arrAlbum[i + 1];
-                            }
-                            System.out.println("Xóa thành công, nhấn 2 để xem lại danh sách.");
-                            break;
-                        } else {
-                            System.out.println("Không có tên CD cần xóa. ");
-                        }
-                    }
-                    n--;
+                    // System.out.println("Nhập tên CD muốn xóa: ");
+                    // String nameRemove = input.next();
+                    // for (int i = 0; i < n; i++) {
+                    // if (arrAlbum[i].tenCD.equals(nameRemove)) {
+                    // for (int j = i; j < n - 1; j++) {
+                    // arrAlbum[j] = arrAlbum[i + 1];
+                    // i++;
+                    // }
+                    // System.out.println("Xóa thành công, nhấn 2 để xem lại danh sách.");
+                    // break;
+                    // } else {
+                    // System.out.println("Không có tên CD cần xóa. ");
+                    // break;
+                    // }
+                    // }
+                    // n--;
+                    __Remove(arrAlbum, n);
                     break;
                 default:
                     System.out.println("GoodBye, See you again!");
@@ -110,4 +118,89 @@ public class App {
 
     }
 
+    protected static void __Input(Album album) {
+        System.out.println("Nhập mã CD: ");
+        album.maCD = input.nextInt();
+        System.out.println("Nhập tên CD: ");
+        album.tenCD = input.next();
+        System.out.println("Nhập tên ca sỹ: ");
+        album.casy = input.next();
+        System.out.println("Nhập số bài hát: ");
+        album.soBH = input.nextInt();
+        System.out.println("Nhập giá tiền: ");
+        album.giaTien = input.nextDouble();
+    }
+
+    public static void __Output(Album album) {
+        System.out.printf("%-10d %-20s %-20s %-20d %-40.1f \n", album.maCD, album.tenCD, album.casy, album.soBH,
+                album.giaTien);
+    }
+
+    public static void __Sum(Album arrAlbum[], int n) {
+        if (arrAlbum.length == 0) {
+            System.out.println("Album trống, chọn 1 để nhập thông tin CD");
+        } else {
+            double sum = 0;
+            for (int i = 0; i < n; i++) {
+                sum += arrAlbum[i].giaTien;
+            }
+            System.out.println("Tổng giá tiền: " + sum);
+        }
+
+    }
+
+    public static void __SortMoney(Album arrAlbum[], int n) {
+        if (arrAlbum.length == 0) {
+            System.out.println("Album trống, chọn 1 để nhập thông tin CD");
+        } else {
+            Album temp = arrAlbum[0];
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    if (arrAlbum[i].giaTien < arrAlbum[j].giaTien) {
+                        temp = arrAlbum[j];
+                        arrAlbum[j] = arrAlbum[i];
+                        arrAlbum[i] = temp;
+                    }
+                }
+            }
+            System.out.println("Sắp xếp hoàn tất, nhấn 2 để xem lại danh sách.");
+        }
+    }
+
+    public static void __SortName(Album arrAlbum[], int n) {
+        if (arrAlbum.length == 0) {
+            System.out.println("Album trống, chọn 1 để nhập thông tin CD");
+        } else {
+            Album temp = arrAlbum[0];
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    if (arrAlbum[i].tenCD.compareTo(arrAlbum[j].tenCD) > 0) {
+                        temp = arrAlbum[j];
+                        arrAlbum[j] = arrAlbum[i];
+                        arrAlbum[i] = temp;
+                    }
+                }
+            }
+            System.out.println("Sắp xếp hoàn tất, nhấn 2 để xem lại danh sách.");
+        }
+    }
+
+    public static void __Remove(Album arrAlbum[], int n) {
+        System.out.println("Nhập tên CD muốn xóa: ");
+        String nameRemove = input.next();
+        for (int i = 0; i < n; i++) {
+            if (arrAlbum[i].tenCD.equals(nameRemove)) {
+                for (int j = i; j < n - 1; j++) {
+                    arrAlbum[j] = arrAlbum[i + 1];
+                    i++;
+                }
+                System.out.println("Xóa thành công, nhấn 2 để xem lại danh sách.");
+                break;
+            } else {
+                System.out.println("Không có tên CD cần xóa. ");
+                break;
+            }
+        }
+        n--;
+    }
 }
